9821e1e70bb00910971f163a75dd5167a315b5c9
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-04-08 22:46:09 -0300

    agregue una inyeccion de dependencia

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using TaskList_API.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//inyeccion de dependecias
+builder.Services.AddScoped<ITaskService, TaskService>();
 
 var app = builder.Build();
 
