4fd86245551c6fafceaf0ddd1f70c062cca5daa5
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-04-08 22:45:17 -0300

    Creé las interfaces con los metodos de los servicios
    las usaré para hacer inyeccion de dependencia

---
+++ Service/ITaskService.cs
@@ -0,0 +1,14 @@
+﻿using TaskList_API.Model;
+
+namespace TaskList_API.Service
+{
+    public interface ITaskService
+    {
+        IEnumerable<TaskModel> GetTasks();
+        TaskModel GetOneTask(Guid id);
+        IEnumerable<TaskModel> GetTaskByUser(Guid userId);
+        Task SaveTask(TaskModel task);
+        Task UpdateTask(Guid id, TaskModel task);
+        Task DeleteTask(Guid id);
+    }
+}
---
+++ Service/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using TaskList_API.Model;
+
+namespace TaskList_API.Service
+{
+    public interface IUserService
+    {
+        IEnumerable<UserModel> GetUsers();
+        Task SaveUser(UserModel user);
+        Task UpdateUser(Guid id, UserModel user);
+        Task DeleteUser(Guid id);
+    }
+}
