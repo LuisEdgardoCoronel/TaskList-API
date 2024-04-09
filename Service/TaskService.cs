1f4b7bbf8487a8dbeb7eeb272cb0e7a4f947c66a
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-04-08 22:42:44 -0300

    Creé los servicios necesarios
    agregue los metodos implementados en las interfaces

---
+++ Service/TaskService.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using TaskList_API.Model;
+
+namespace TaskList_API.Service
+{
+    public class TaskService:ITaskService
+    {
+        TaskContext context;
+
+        public TaskService(TaskContext taskContext)
+        {
+            this.context = taskContext;
+        }
+
+
+
+        public IEnumerable<TaskModel> GetTasks() 
+        {
+            return context.Tasks;
+        }
+
+
+
+        public TaskModel GetOneTask(Guid id)
+        {
+            return context.Tasks.Find(id);
+        }
+
+
+
+
+        public IEnumerable<TaskModel> GetTaskByUser(Guid userId)
+        {
+            return context.Tasks.Where(x => x.UserId == userId);
+        }
+
+
+
+
+        public async Task SaveTask(TaskModel task)
+        {
+            context.Add(task);
+            await context.SaveChangesAsync();
+        }
+
+
+
+
+
+        public async Task UpdateTask(Guid id, TaskModel task)
+        {
+            var taskSearched = context.Tasks.Find(id);
+            if (taskSearched != null)
+            {
+                taskSearched.TaskName = task.TaskName;
+                taskSearched.TaskDescription = task.TaskDescription;
+                taskSearched.ImportanceOfTask = task.ImportanceOfTask;
+
+                await context.SaveChangesAsync();
+            }
+        }
+
+
+
+
+
+        public async Task DeleteTask(Guid id)
+        {
+            var taskSearched = context.Tasks.Find(id);
+            if (taskSearched != null)
+            {
+                context.Remove(taskSearched);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Service/UserService.cs
@@ -0,0 +1,57 @@
+﻿using TaskList_API.Model;
+
+namespace TaskList_API.Service
+{
+    public class UserService:IUserService
+    {
+        TaskContext context;
+        public UserService(TaskContext context)
+        {
+            this.context = context;
+        }
+
+
+
+        public IEnumerable<UserModel> GetUsers()
+        {
+            return context.Users;
+        }
+
+
+
+
+        public async Task SaveUser(UserModel user)
+        {
+            context.Add(user);
+            await context.SaveChangesAsync();
+        }
+
+
+
+
+        public async Task UpdateUser(Guid id, UserModel user)
+        {
+            var userSearched = context.Users.Find(id);
+            if (userSearched != null)
+            {
+                userSearched.UserName = user.UserName;
+                userSearched.Password = user.Password;
+
+                await context.SaveChangesAsync();
+            }
+        }
+
+
+
+
+        public async Task DeleteUser(Guid id)
+        {
+            var userSearched = context.Users.Find(id);
+            if (userSearched != null)
+            {
+                context.Remove(userSearched);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
