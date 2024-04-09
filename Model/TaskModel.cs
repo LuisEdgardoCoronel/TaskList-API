a75c1c154e28eedd9454fcc3f84476595601542d
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-04-08 22:41:21 -0300

    Creacion de los modelos
    Agregue los atributos necesarios para la creacion de la base de datos

---
+++ Model/TaskModel.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskList_API.Model
+{
+    public class TaskModel
+    {
+        public Guid TaskId { get; set; }
+        public Guid UserId { get; set; }
+        public string TaskName { get; set; }
+        public int TaskDescription { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public ImportanceOfTask ImportanceOfTask { get; set; }
+        public UserModel User { get; set; } //para clave forane
+
+    }
+}
