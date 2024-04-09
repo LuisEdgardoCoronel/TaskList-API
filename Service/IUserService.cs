eb1773b27f0640dba82367cdc8cba92b0aa67e79
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-04-09 20:30:35 -0300

    agregue el metodo faltante

--- Service/IUserService.cs
+++ Service/IUserService.cs
@@ -5,6 +5,7 @@ namespace TaskList_API.Service
     public interface IUserService
     {
         IEnumerable<UserModel> GetUsers();
+        UserModel GetOneUser(Guid id);
         Task SaveUser(UserModel user);
         Task UpdateUser(Guid id, UserModel user);
         Task DeleteUser(Guid id);
