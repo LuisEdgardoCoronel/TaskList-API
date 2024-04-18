d54b515d25dbf2d31f36b5024fa7b44908c01455
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-04-18 01:52:32 -0300

    Create DbConectionController.cs
    created database

---
+++ Controllers/DbConectionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskList_API.Controllers
+{
+    [ApiController]
+    [Route("/api/[Controller]")]
+    public class DbConectionController:ControllerBase
+    {
+        TaskContext dbcontext;
+        public DbConectionController(TaskContext dbcontext)
+        {
+            this.dbcontext = dbcontext;
+        }
+
+        [HttpGet]
+        [Route("createdb")]
+        public IActionResult CreateDb()
+        {
+            dbcontext.Database.EnsureCreated();
+            return Ok();
+        }
+
+
+    }
+}
