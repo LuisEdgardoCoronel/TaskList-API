3fbf99c6ed1902551e0471fcf4e5d065d76672a0
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-04-18 01:52:08 -0300

    create middlewares
    error and logging

---
+++ Middleware/ErrorMiddleware.cs
@@ -0,0 +1,41 @@
+﻿namespace TaskList_API.Middleware
+{
+    public class ErrorMiddleware
+    {
+        readonly RequestDelegate next;
+
+        public ErrorMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception)
+            {
+                // Manejar la excepción y generar una respuesta adecuada
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                await context.Response.WriteAsync("Internal Server Error");
+
+                // Registrar el error en un archivo de registro con Serilog o NLog
+                // ej: logger.LogError(ex, "Error en la API");
+            }
+        }
+    }
+
+
+
+
+
+    public static class ErrorMiddlewareExtension
+    {
+        public static IApplicationBuilder UseErrorMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<ErrorMiddleware>();
+        }
+    }
+}
---
+++ Middleware/LoggingMiddleware.cs
@@ -0,0 +1,39 @@
+﻿namespace TaskList_API.Middleware
+{
+    public class LoggingMiddleware
+    {
+        readonly RequestDelegate next;
+        private readonly ILogger<LoggingMiddleware> logger;
+
+        public LoggingMiddleware(ILogger<LoggingMiddleware> logger, RequestDelegate next) 
+        { 
+            this.logger = logger;
+            this.next = next;
+        }
+
+        public async void Invoke(HttpContext context)
+        {
+            //registrar informacion entrante
+            logger.LogInformation($"Request:{context.Request.Method} {context.Request.Path} from {context.Connection.RemoteIpAddress} at {DateTime.UtcNow}");
+
+            await next(context);
+
+            //registrar informacion saliente
+            logger.LogInformation($"Response: {context.Response.StatusCode} for {context.Request.Method} {context.Request.Path} at {DateTime.UtcNow}");
+        }
+    }
+
+
+    
+
+
+
+
+    public static class LoggingMiddlewareExtension
+    {
+        public static IApplicationBuilder UseLoggingMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<LoggingMiddleware>();
+        }
+    }
+}
