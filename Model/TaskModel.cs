57b2f595ba47f5939b41ca59fd19294a3caf4a28
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-04-26 21:42:00 -0300

    agregue taskdto
    una clase taskdto para facilitar la recepcion de datos en la api

--- Model/TaskModel.cs
+++ Model/TaskModel.cs
@@ -1,14 +1,26 @@
-﻿namespace TaskList_API.Model
+﻿using System.Text.Json.Serialization;
+
+namespace TaskList_API.Model
 {
     public class TaskModel
     {
         public Guid TaskId { get; set; }
         public Guid UserId { get; set; }
         public string TaskName { get; set; }
-        public int TaskDescription { get; set; }
+        public string TaskDescription { get; set; }
         public DateTime CreatedDate { get; set; }
         public ImportanceOfTask ImportanceOfTask { get; set; }
-        public UserModel User { get; set; } //para clave forane
+        public virtual UserModel User { get; set; } //para clave foranea
+
+    }
+
+    public class TaskDto//data transfer Object
+    {
+        public Guid TaskId { get; set;}
+        public Guid UserId { get; set;}
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public ImportanceOfTask ImportanceOfTask { get; set;}
 
     }
 }
